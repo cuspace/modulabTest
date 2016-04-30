30065eb9b953a0308fa3430a75c45d8c5ac9be22
cuspace <cuspace@gmail.com>
2016-04-30 19:43:04 +0900

    require 를 사용할 것이고 js 폴더를 사용할 것이다. var 대신 const 를 사용

--- index.ios.js
+++ index.ios.js
@@ -4,51 +4,9 @@
  * @flow
  */
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+'use strict';
 
-class modulabTest extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          모두의 연구소
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const {AppRegistry} = require('react-native');
+const app = require('./js/app');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    color: 'deeppink',
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('modulabTest', () => modulabTest);
+AppRegistry.registerComponent('modulabTest', () => app);
